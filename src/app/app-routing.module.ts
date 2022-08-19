c3c1a224f6161a05c424291336d32db250cc9e85
Alexis Gonzalez <1991.alexis@gmail.com>
2022-08-19 15:53:35 -0400

    Added skills card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CardsComponent } from './cards/cards.component';
+import { SkillsCardComponent } from './skills-card/skills-card.component';
 
 const routes: Routes = [
   { path: '',   redirectTo: '/main-page', pathMatch: 'full' },
   { path: 'main-page', component: MainPageComponent },
   { path: 'cards-page', component: CardsComponent},
+  { path: 'skills-page', component: SkillsCardComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
