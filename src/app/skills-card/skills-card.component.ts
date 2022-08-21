c91bd5b59104bf9ee855be376ed46c9e8fb24426
Alexis Gonzalez <1991.alexis@gmail.com>
2022-08-20 22:22:11 -0400

    now char updates when i want

--- src/app/skills-card/skills-card.component.ts
+++ src/app/skills-card/skills-card.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class SkillsCardComponent implements OnInit {
 
+  data = [
+    [
+      //Grecia
+      { axis: "Liderazgo", value: 0.2 },
+      { axis: "Arq. Software", value: 0.2 },
+      { axis: "Ciencia de Datos", value: 0.2 },
+      { axis: "UI/UX", value: 0.1 },
+      { axis: "Frontend", value: 0.8 },
+      { axis: "Backend", value: 0.5 }
+    ]
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -14,6 +26,17 @@ export class SkillsCardComponent implements OnInit {
 
   changeGraphData() {
     console.log("locl");
+    this.data = [
+    [
+      //Grecia
+      { axis: "Leadership", value: 0.8 },
+      { axis: "Soft. Arq", value: 0.3 },
+      { axis: "Data Science", value: 0.7 },
+      { axis: "UX/UI", value: 0.4 },
+      { axis: "Front", value: 0.1 },
+      { axis: "Back", value: 0.9 }
+    ]
+  ];
   }
 
 }
