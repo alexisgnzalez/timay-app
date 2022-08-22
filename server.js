11632735435b0117c912d918f9c711ea2f87e1cc
Alexis Gonzalez <1991.alexis@gmail.com>
2022-08-22 17:42:35 -0400

    Compiled but server port was incorrect

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/<timay-app>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/<timay-app>/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
