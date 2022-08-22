07429c4d6eb48f051a290c4fb5807d8d3c0c1f13
Alexis Gonzalez <1991.alexis@gmail.com>
2022-08-22 17:32:25 -0400

    updates to deploy to keroku

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.listen(process.env.PORT || 4200);
+app.use(express.static(__dirname + '/dist/<timay-app>'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/<timay-app>/index.html'));});
+app.listen(process.env.PORT || 8080);
